a626768a11ea26f6773d467031c569b81a5d9dbe
rmmsd2 <rmmsd@iscte-iul.pt>
2025-11-28 02:16:43 +0000

    Adição de análise exploratória e dependências necessárias
    análise das variáveis com % vazios, "N/A" e "None" nas strings e cargos.
    Gráficos

--- r/01_data_understanding.R
+++ r/01_data_understanding.R
@@ -1,7 +1,7 @@
+##################################################
+# === 1. Recolha e decrição de dados inicial === #
+##################################################
 
-########################################################
-# === 1. Preparação do ambiente e pacotes necessários ===
-########################################################
 # Carregar dependências e definir seed
 source("r/dependencies.R")
 
@@ -16,51 +16,86 @@ cat("Dimensão:", dim(df_raw), "\n")
 # Sumário expandido com a função skim (já oferece os tipos de variáveis)
 skim(df_raw)
 
-# percentagem NA por coluna
-na_percentage <- sapply(df_raw, function(x) mean(is.na(x)))*100
-na_percentage_sorted <- sort(na_percentage, decreasing = TRUE)
-cat("% NAs / Column (Sorted):\n")
-print(na_percentage_sorted)
+# Visão geral da estrutura dos dados
+glimpse(df)
+
+
+#################################
+# === 2. Análise exploratória === #
+#################################
+
+### Entradas vazias ###
+
+# Calcular % de NA ou Vazio
+missing_simple <- df_raw %>%
+  summarise(across(everything(), ~ mean(is.na(.) | as.character(.) == "") * 100)) %>%
+  pivot_longer(cols = everything(), names_to = "Variavel", values_to = "Pct_Vazios") %>%
+  arrange(desc(Pct_Vazios))
+
+# Mostrar o resultado formatado
+print(as.data.frame(missing_simple))
+
+# Gráfico de barras % Dados em falta por variável
+ggplot(filter(missing_simple, Pct_Vazios > 0), 
+       aes(x = reorder(Variavel, Pct_Vazios), y = Pct_Vazios)) +
+  geom_bar(stat = "identity", fill = "tomato") +
+  coord_flip() + # Barra horizontal para ler melhor os nomes
+  labs(title = "% de dados vazios por Variável",
+       x = "Variável",
+       y = "Percentagem (%)") +
+  theme_minimal()
+
+### Entradas com dados vazios ###
+
+# Contar quantas vezes aparecem strings que representam nulos mas não são NA reais
+count_hidden_na <- function(column, pattern) {
+  sum(str_count(as.character(column), pattern), na.rm = TRUE)
+}
+
+# Verificar "N/A"
+na_text_counts <- sapply(df_raw, count_hidden_na, pattern = "N/A")
+print(head(sort(na_text_counts, decreasing = TRUE), 5))
+
+# Verificar "None"
+none_text_counts <- sapply(df_raw, count_hidden_na, pattern = "None")
+print(head(sort(none_text_counts, decreasing = TRUE), 5))
+
+
+### estatística e distribuições ###
+
+## X.job_position_name ##
+
+# Tabela de Frequências
+job_counts <- df_raw %>%
+  count(X.job_position_name, sort = TRUE)
+
+print("Número de Cargos Únicos:")
+print(nrow(job_counts))
+
+print("Cargos existentes:")
+print(head(job_counts, nrow(job_counts)))
+
+
+## matched_score ##
 
 # confirmar a variável dependente 
 table(is.na(df_raw$matched_score))
 
-# explorar distribuição do matched_score - Histograma
+# Analisar matched_score
+summary(df$matched_score)
+stat.desc(df$matched_score)
 
-windows()  # abrir nova janela
-hist(df_raw$matched_score, breaks = 30, main = "Distribuição matched_score", xlab = "matched_score")
+# distribuição do Matched Score - Histograma
+ggplot(df_raw, aes(x = matched_score)) +
+  geom_histogram(binwidth = 0.05, fill = "tomato", color = "black") +
+  labs(title = "Distribuição do Matched Score",
+       x = "Score",
+       y = "Frequência") +
+  theme_minimal()
 
 # descrição de outliers a partir do boxplot 
-
-windows()  # abrir nova janela
-boxplot(df_raw$matched_score, main="Boxplot matched_score", ylab="matched_score")
+boxplot(df_raw$matched_score, main="Boxplot matched_score", ylab="matched_score", col = "tomato")
 
 # deteção de outliers a partir do critério de IQR (Intervalo Interquartil)
-boxplot.stats(df_raw$matched_score)$out
-
-
-########################################################
-# === 3. Variável Dependente matched_score ===
-########################################################
-
-df_raw$matched_score <- as.numeric(df_raw$matched_score)
-
-# numero em falta na var dependente
-cat("NAs em matched_score:", sum(is.na(df_raw$matched_score)), "\n")
-
-windows()  # abrir nova janela
-hist(df_raw$matched_score, breaks = 30,
-     main = "Distribuição matched_score",
-     col = "skyblue", xlab = "matched_score", border="white")
-
-
-# Boxplot
-windows()  # abrir nova janela
-boxplot(df_raw$matched_score,
-        main = "Boxplot matched_score",
-        col = "orange", ylab = "matched_score")
-
-# Outliers via IQR (interquartile range)
 outliers <- boxplot.stats(df_raw$matched_score)$out
 cat("Outliers detectados:", length(outliers), "\n")
-
--- r/dependencies.r
+++ r/dependencies.r
@@ -13,7 +13,7 @@ packages <- c(
   "tidyverse", "caret", "randomForest", "xgboost", 
   "text2vec", "glmnet", "recipes", "rsample", "skimr", "vip", "caret", 
   "skimr", "stringr", "corrplot", "dplyr", "bestNormalize", "moments", 
-  "gridExtra", "ggplot2"
+  "gridExtra", "ggplot2", "pastecs"
 )
 
 # 2. Instalação de Pacotes
@@ -34,6 +34,7 @@ library(stringr)
 library(skimr)
 library(corrplot)
 library(dplyr)
+library(pastecs)
 
 # Definir seed data para reproducibilidade do script.
 set.seed(123)
